3392284b816473999d948d11922767590fe8c614
Carlos Alejandro Murga Martínez <carlos.murga1@catolica.edu.sv>
2024-04-24 18:11:25 -0600

    Sección "Combos": Se ha comentado todo el código del controlador para mayor comprensión de cómo funciona.

--- Modulo1_Administracion/Controllers/combosController.cs
+++ Modulo1_Administracion/Controllers/combosController.cs
@@ -1,21 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Firebase.Auth;
 using Firebase.Storage;
 using Modulo1_Administracion.Models;
-using static System.Formats.Asn1.AsnWriter;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Modulo1_Administracion.Data;
-using Azure.Identity;
-using System.Diagnostics.Metrics;
-using Microsoft.CodeAnalysis.Elfie.Diagnostics;
-using Microsoft.Data.SqlClient;
 using System.Data;
 
 namespace Modulo1_Administracion.Controllers
@@ -32,47 +20,36 @@ namespace Modulo1_Administracion.Controllers
         // GET: combos
         public async Task<IActionResult> Index(int? numPag)
         {
-            //Aquí estamos invocando el listado de puestos de la tabla puestos
+            //Llama a todos los registros de la tabla items_menu para mostrarlas en el combobox "Items".
             var listaDePlatos = (from m in _DulceSaborContext.items_menu
                                   select m).ToList();
             ViewData["listadoDePlatos"] = listaDePlatos;
 
+            //Llama a todos los registros de la tabla estados para mostrarlas en el combobox "Estado".
             var listaDeEstados = (from m in _DulceSaborContext.estados
                                  select m).ToList();
             ViewData["listadoDeEstados"] = listaDeEstados;
 
-            //Aquí estamos invocando el listado de departamentos de la tabla departamentos
-            //var listaDeDepartamentos = (from m in _DulceSaborContext.departamentos
-            //                            select m).ToList();
-            //ViewData["listadoDeDepartamentos"] = new SelectList(listaDeDepartamentos, "id", "departamento");
-
-
-
-            //Aquí estamos solicitando el listado de los Departamentos en la bd
-            var combosL = (from m in _DulceSaborContext.combos
+            //Llama a todos los registros de la tabla combos para mostrarlas en la tabla de la vista index de combos.
+            var listadoDeCombos = (from m in _DulceSaborContext.combos
                                   select m);
-            ViewData["combosL"] = combosL;
-
-            var listadoDeCombos = (from c in _DulceSaborContext.combos
-                                       select new
-                                       {
-                                           id = c.id_combo,
-                                           nombre = c.descripcion,
-                                           precio = c.precio
-                                       });
-            ViewData["listadoDeCombos"] = listadoDeCombos.ToList();
 
             int cantidadRegistros = 15;
 
-            return View(await Paginacion<combos>.CrearPaginacion(combosL.AsNoTracking(), numPag?? 1, cantidadRegistros));
+            //return View(await
+            //Paginacion<combos>.CrearPaginacion( -> Paginacion -> Nombre de la clase -> <combos> -> <nombre del modelo al que se le va a hacer paginacion>
+            //numPag?? 1 -> número de página (Viene de los botones de paginación del index)
+            //cantidadRegistros -> Cuantos registros se van a mostrar por cada paginación
+            //))
+
+            //La clase "Paginacion" funciona para todos los controladores, solo copien el código del return View y modifiquen lo de "listadoDeCombos".
+            return View(await Paginacion<combos>.CrearPaginacion(listadoDeCombos.AsNoTracking(), numPag?? 1, cantidadRegistros));
         }
 
-        // GET: combos/Create
         [HttpPost]
         public async Task<ActionResult> CreateCombo(IFormFile archivo, string nombre, string platos, decimal precio, int id_estado)
         {
-            int id_combo = 0;
-            combos combosObj = new combos();
+            /*  INICIO GUARDADO DE LA IMAGEN EN FIREBASE STORAGE   */
 
             //Leemos el archivo subido
             Stream archivoASubir = archivo.OpenReadStream();
@@ -98,65 +75,92 @@ namespace Modulo1_Administracion.Controllers
 
             var urlArchivoCargado = await tareaCargarArchivo;
 
+            /*  FIN GUARDADO DE LA IMAGEN EN FIREBASE STORAGE   */
 
-            try
-            {
+            /*  INICIO GUARDADO DEL NUEVO REGISTRO "COMBO"   */
+            
+            //Se crea este objeto para poder guardar el nuevo registro
+            combos combosObj = new combos();
 
-
-            }
-            catch (Exception ex) { ex.ToString(); }
+            //Se asignan los valores del nuevo registro a los atributos del objeto
+            //objeto.atributo = variable_recibida_en_este_metodo (CreateCombo);
 
             combosObj.descripcion = nombre;
             combosObj.precio = precio;
             combosObj.imagen = urlArchivoCargado;
             combosObj.id_estado = id_estado;
 
+            //Se hace el INSERT INTO del registro a la tabla Combos.
             _DulceSaborContext.combos.Add(combosObj);
             _DulceSaborContext.SaveChanges();
 
-            var listadoDeCombos = (from c in _DulceSaborContext.combos
-                                   select new
-                                   {
-                                       id = c.id_combo,
-                                       nombre = c.descripcion,
-                                       precio = c.precio
-                                   }).ToList();
-            ViewData["listadoDeCombos"] = listadoDeCombos;
+            /*  FIN GUARDADO DEL NUEVO REGISTRO "COMBO"   */
 
-            foreach (var item in (IEnumerable<dynamic>)ViewData["listadoDeCombos"])
+            /*  INICIO GUARDADO DE TODOS LOS ITEMS(PLATOS) A UN SOLO COMBO   */
+
+            //Lo que se hace es buscar el ID del último combo creado, o sea, el que necesitamos
+            var listadoDeCombos = (from c in _DulceSaborContext.combos
+                                   select c).ToList();
+
+            //Variable donde se asignará el ID del último combo
+            int id_combo = 0;
+
+            //Se recorre toda la consulta hasta encontrar el ID requerido
+            foreach (var item in (IEnumerable<dynamic>)listadoDeCombos)
             {
-                id_combo = item.id;
+                id_combo = item.id_combo;
             }
 
+            //La variable "platos" es donde están todos los items elegidos para el combo
+            //Lo que contiene es, por ejemplo: 4,5,4, | Cada número es el ID del item, pero hay un problema, el último número termina con una coma
+            //Esto hace que se tome en cuenta el espacio después de la coma y haya un error. Por eso se hace lo siguiente:
+            //platos.TrimEnd(',').Split(); -> .TrimEnd(',') -> Elimina la última coma. -> .Split(',') -> Divide la cadena por comas.
+            //O sea que, en lugar de que 4,5,4, se cuente como una cadena, en realidad se toma como un array de 3 sin la coma final
 
-            string numPlatos = platos.ToString();
-            string[] numArrayPlatos = numPlatos.TrimEnd(',').Split(',');
+            //Se elimina la coma final y se divide la cadena. El resultado se está guardando en un Array de Strings
+            string[] numArrayPlatos = platos.TrimEnd(',').Split(',');
+
+            //Los ID's ingresados a numArrayPlatos son String ya que vienen de un text area y llevan coma, entonces, necesitamos convertirlos a INT.
+            //Para eso creamos este nuevo Array que servirá para la conversión. En new int[numArrayPlatos.Length] le estamos indicando el tamaño
+            //que tendrá el array int según la cantidad de items que vayan al combo.
             int[] numIntPlatos = new int[numArrayPlatos.Length];
 
+            //¿Para que necesitamos un FOR?
+            //Para recorrer todos los numArrayPlatos (o sea, los ID's de los items), convertirlos string a int y hacer un INSERT INTO a la tabla
+            //por cada item añadido al combo.
             for (int i = 0; i < numArrayPlatos.Length; i++)
             {
+                //Se crea un objeto para ayudarnos a ingresar los registros.
                 items_combo_dos items = new items_combo_dos();
 
+                //Aquí se hace la conversión
                 numIntPlatos[i] = Int32.Parse(numArrayPlatos[i]);
 
+                //Aquí se asignan los valores para cada campo de la tabla.
                 items.id_combo = id_combo;
                 items.id_items_menu = numIntPlatos[i];
 
+                //Y, por último, se hace el INSERT INTO por cada item añadido.
                 _DulceSaborContext.items_combo_dos.Add(items);
                 _DulceSaborContext.SaveChanges();
             }
 
+            /*  FIN GUARDADO DE TODOS LOS ITEMS(PLATOS) A UN SOLO COMBO   */
+
+            //Al final redirige al método Index, donde está la vista Index.
             return RedirectToAction("Index");
         }
 
-        public IConfiguration Configuration { get; }
-
         public async Task<IActionResult> Details(int? id, int? numPag)
         {
+            //Se hace el llamado a la tabla estados para mostrar todos los estados disponibles en el combobox "Estado:".
             var listaDeEstados = (from m in _DulceSaborContext.estados
                                   select m).ToList();
+
+            //ViewData creado para poder usarlo en la vista Details.cshtml
             ViewData["listadoDeEstados"] = listaDeEstados;
 
+            //Se llama al registro del cual se requieren sus campos
             var combo = (from c in _DulceSaborContext.combos
                          .Where(c => c.id_combo == id)
                          join e in _DulceSaborContext.estados on c.id_estado equals e.id_estado
@@ -168,6 +172,8 @@ namespace Modulo1_Administracion.Controllers
                              estado = e.nombre
                          }).ToList();
 
+            //Se hace el ViewData para usarlo en la vista Details.cshtml, pero este es diferente.
+            //Ya que la consulta solo es de un registro podemos hacer un objeto para usar sus atributos más fácil en la vista.
             ViewData["infoCombo"] = new obj_combo_estado()
             {
                 id_combo = combo.AsEnumerable().FirstOrDefault().id,
@@ -176,10 +182,14 @@ namespace Modulo1_Administracion.Controllers
                 estado = combo.AsEnumerable().FirstOrDefault().estado
             };
 
+            //Este es un llamado a una tabla, pero no cualquier tabla.
+            //Esta llamando a una "VISTA" ("VIEW") de la base de datos. Para esto, también, se necesita crear una clase como modelo.
+            //Como si fuese una tabla real.
             var listadoDeItemsCombos = (from otc in _DulceSaborContext.obj_items_combos
                                         where otc.id_combo == id
                                         select otc);
 
+            //VieWData creado para poder usarlo en la vista Details.cshtml
             ViewData["listadoDeItemsCombos"] = listadoDeItemsCombos;
 
             int cantidadRegistros = 6;
@@ -190,6 +200,8 @@ namespace Modulo1_Administracion.Controllers
         [HttpPost]
         public async Task<IActionResult> EditCombo(int id, [Bind("id_combo, descripcion, precio, id_estado")] combos combos)
         {
+            //Aquí no hace falta hacer un objeto con sus asignaciones. Ya que es una actualización (también se puede para crear registros)
+            //a una sola tabla. Los datos los toma desde los atributos que están en Bind en la parte de los parámetros de este metodo "EditCombo".
             _DulceSaborContext.Update(combos);
             await _DulceSaborContext.SaveChangesAsync();
 
