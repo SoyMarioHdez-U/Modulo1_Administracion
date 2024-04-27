0b1c7a173c406c30a32a1c37a6286c4284ca6389
Carlos Alejandro Murga Martínez <carlos.murga1@catolica.edu.sv>
2024-04-27 01:27:15 -0600

    Commit: paginación de la tabla del index "Index_mesa" arreglado.

--- Modulo1_Administracion/Data/v_mesas_estado.cs
+++ Modulo1_Administracion/Data/v_mesas_estado.cs
@@ -7,7 +7,7 @@ namespace Modulo1_Administracion.Data
         public int id_mesa { get; set; }
         public int id { get; set; }
         public int? cantidad_personas { get; set; }      
-        public int? estado { get; set; }
+        public string? estado { get; set; }
         public string? nombre_mesa { get; set; }
     }
 }
